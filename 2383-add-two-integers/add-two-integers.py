9b4569643f253f31f356889a768f8cb88413c2c6
Mekdes Assefa <mekdes.asefa-ug@aau.edu.et>
2025-07-15 11:35:34 +0300

    Time: 3 ms (2.18%) | Memory: 17.7 MB (72.72%) - LeetSync

---
+++ 2383-add-two-integers/add-two-integers.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        '''
+        sum
+        '''
+        num1 = int(num1)
+        num2 = int(num2)
+        return num1 + num2
+        
