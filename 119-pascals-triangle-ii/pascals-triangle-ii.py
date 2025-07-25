d5a7080e84d484baaf899fc6e179e1f8ff3bfcb2
Mekdes Assefa <mekdes.asefa-ug@aau.edu.et>
2025-07-25 13:31:05 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (11.74%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,17 +1,17 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
+        
         if rowIndex == 0:
             return [1]
         
-        # Recursive call to get previous row
         prev_row = self.getRow(rowIndex - 1)
-        
-        # Build current row by adding adjacent elements of previous row
-        current_row = [1]  # first element is always 1
-        
+        curr_row = [1]
+
         for i in range(1, len(prev_row)):
-            current_row.append(prev_row[i - 1] + prev_row[i])
-            
-        current_row.append(1)  # last element is always 1
+            curr_row.append(prev_row[i - 1] + prev_row[i])
+
+        curr_row.append(1)
+        return curr_row
         
-        return current_row
+
+        
