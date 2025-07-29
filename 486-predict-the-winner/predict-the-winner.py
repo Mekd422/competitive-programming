420850ddfca5ff4002cdfd1c09caa1b607808190
Mekdes Assefa <mekdes.asefa-ug@aau.edu.et>
2025-07-29 10:39:21 +0300

    Time: 3 ms (69.87%) | Memory: 18.1 MB (28.77%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,16 +1,19 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        from functools import lru_cache
-
-        @lru_cache(None)
+        
+        memo = {}
         def helper(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
             if i == j:
                 return nums[i]
 
             pick_left = nums[i] - helper(i + 1, j)
             pick_right = nums[j] - helper(i, j -1)
 
-            return max(pick_left, pick_right)
+            res =  max(pick_left, pick_right)
+            memo[(i, j)] = res
+            return res
 
         return helper(0, len(nums)  - 1) >= 0
 
