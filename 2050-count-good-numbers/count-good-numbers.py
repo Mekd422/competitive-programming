382f669e4d54ddc2836ae11098e10be6506cda7c
Mekdes Assefa <mekdes.asefa-ug@aau.edu.et>
2025-07-30 11:28:13 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (24.48%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -3,19 +3,16 @@ class Solution:
         MOD = 10**9 + 7 
 
         def pow(x, n):
-
             if n == 0:
                 return 1
-            res = 1
-            while n > 1:
-                if (n % 2):
-                    res = (res * x) % MOD
-                n = n // 2
-                x = (x * x) % MOD
-            return res * x
+
+            half = pow(x, n // 2)
+            if n % 2 == 0:
+                return (half * half) % MOD
+            else:
+                return (half * half * x) % MOD
 
         even = ceil(n / 2)
         odd = n // 2
 
         return (pow(5, even) * pow(4, odd)) % MOD
-        
