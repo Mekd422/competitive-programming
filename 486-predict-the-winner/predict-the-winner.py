3b23c203441669b602e40a1317da2a5a6aa01077
Mekdes Assefa <mekdes.asefa-ug@aau.edu.et>
2025-07-30 11:00:12 +0300

    Time: 2371 ms (17.95%) | Memory: 17.7 MB (74.61%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,20 +1,16 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        
-        memo = {}
+
         def helper(i, j):
-            if (i, j) in memo:
-                return memo[(i, j)]
-            if i == j:
+            if i == j :
                 return nums[i]
 
-            pick_left = nums[i] - helper(i + 1, j)
-            pick_right = nums[j] - helper(i, j -1)
+            left = nums[i] - helper(i + 1, j)
+            right = nums[j] - helper(i, j - 1)
 
-            res =  max(pick_left, pick_right)
-            memo[(i, j)] = res
-            return res
+            return max(left, right)
 
-        return helper(0, len(nums)  - 1) >= 0
+        return (helper(0, len(nums) - 1)) >= 0
 
+        
         
