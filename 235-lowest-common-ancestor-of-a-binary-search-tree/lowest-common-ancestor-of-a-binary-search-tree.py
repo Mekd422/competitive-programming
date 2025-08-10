aeae1ddb71934fa591871119fac3c7f5de389664
Mekdes Assefa <mekdes.asefa-ug@aau.edu.et>
2025-08-10 12:33:06 +0300

    Time: 53 ms (84.90%) | Memory: 21.1 MB (20.31%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,22 +7,29 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        ''''''
-        if not root or root == p or root == q:
-            return root
-        
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
+        # if not root:
+        #     return 
 
-        if left and right:
-            return root
+        # if root == p or root == q:
+        #     return root
+        # if root.left == p and root.right == q or root.left == q and root.right == p:
+        #     return root
+        # if root.left == p and root != q:
+        #     return root.left
+        # if root.left == q or root != q:
+        #     return root.right
 
-        return left if left else right
+        # return self.lowestCommonAncestor(root.left)
+        # return self.lowestCommonAncestor(root.right)
 
-        
+        curr = root
+        while curr:
+            if curr.val < p.val and curr.val < q.val:
+                curr = curr.right
+            elif curr.val > p.val and curr.val > q.val:
+                curr = curr.left
+            else:
+                return curr
 
 
-
-        
-        
         
